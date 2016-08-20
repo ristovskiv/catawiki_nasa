571dfe00995667413e6fe1475106f0134a83497b
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2016-08-20 14:11:09 +0200

    feat: rover spinning left or right 90 degrees

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,12 +1,38 @@
 class Rover
-  attr_accessor :x, :y
+  attr_accessor :x, :y, :compass_point
 
-  CARDINAL_COMPASS_POINTS = [:N, :E, :S, :W]
+  CARDINAL_COMPASS_POINTS = {
+    :N => {left: :W, right: :E},
+    :E => {left: :N, right: :S},
+    :S => {left: :E, right: :W},
+    :W => {left: :S, right: :N}
+  }
 
   def initialize(x, y, compass_point)
     @x = x
     @y = y
     @compass_point = validate_cardinal_compass_point(compass_point)
+  end
+
+  def spin_left
+    @compass_point = CARDINAL_COMPASS_POINTS[compass_point][:left]
+  end
+
+  def spin_right
+    @compass_point = CARDINAL_COMPASS_POINTS[compass_point][:right]
+  end
+
+  def move
+    case compass_point
+    when :N
+      go_north!
+    when :E
+      go_east!
+    when :S
+      go_south!
+    when :W
+      go_west!
+    end
   end
 
   def go_east!
@@ -28,7 +54,7 @@ class Rover
   private
 
   def validate_cardinal_compass_point(compass_point)
-    raise CardinalCompassPointError.new('Invalid cardinal compass point') unless CARDINAL_COMPASS_POINTS.include?(compass_point)
+    raise CardinalCompassPointError.new('Invalid cardinal compass point') unless CARDINAL_COMPASS_POINTS.keys.include?(compass_point)
     compass_point
   end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -13,7 +13,7 @@ describe Rover do
 
   context 'validation compass point' do
     it 'is valid with compass point N, E, S or W' do
-      expect{Rover.new(1,2, Rover::CARDINAL_COMPASS_POINTS.sample)}.to_not raise_error(CardinalCompassPointError)
+      expect{Rover.new(1,2, Rover::CARDINAL_COMPASS_POINTS.keys.sample)}.to_not raise_error(CardinalCompassPointError)
     end
 
     it 'is not valid with non cardinal compass point letters' do
@@ -24,46 +24,113 @@ describe Rover do
   context 'moving' do
     let(:rover){ Rover.new(1,2, :N)}
 
-    describe 'east' do
-      it 'incremets x coordinate' do
-        expect{rover.go_east!}.to change(rover, :x).by(1)
+    context 'methods' do
+      describe 'east' do
+        it 'incremets x coordinate' do
+          expect{rover.go_east!}.to change(rover, :x).by(1)
+        end
+
+        it 'not change the y coordinate' do
+          expect{rover.go_east!}.not_to change(rover, :y)
+        end
       end
 
-      it 'not change the y coordinate' do
-        expect{rover.go_east!}.not_to change(rover, :y)
+      describe 'west' do
+        it 'decrements x coordinate' do
+          expect{rover.go_west!}.to change(rover, :x).by(-1)
+        end
+
+        it 'not change the y coordinate' do
+          expect{rover.go_west!}.not_to change(rover, :y)
+        end
+      end
+
+      describe 'north' do
+        it 'not change x coordinate' do
+          expect{rover.go_north!}.not_to change(rover, :x)
+        end
+
+        it 'increments the y coordinate' do
+          expect{rover.go_north!}.to change(rover, :y).by(1)
+        end
+      end
+
+      describe 'south' do
+        it 'not change x coordinate' do
+          expect{rover.go_north!}.not_to change(rover, :x)
+        end
+
+        it 'decrements the y coordinate' do
+          expect{rover.go_south!}.to change(rover, :y).by(-1)
+        end
       end
     end
 
-    describe 'west' do
-      it 'decrements x coordinate' do
-        expect{rover.go_west!}.to change(rover, :x).by(-1)
+  end
+
+  context 'spinning' do
+    let(:rover){ Rover.new(1,2, :N)}
+    describe 'starting from North' do
+      describe 'spin left' do
+        specify '90 degrees' do
+          expect{rover.spin_left}.to change(rover, :compass_point).from(:N).to(:W)
+        end
+
+        specify '180 degrees' do
+          expect do
+            2.times do
+              rover.spin_left
+            end
+          end.to change(rover, :compass_point).from(:N).to(:S)
+        end
+
+        specify '270 degrees' do
+          expect do
+            3.times do
+              rover.spin_left
+            end
+          end.to change(rover, :compass_point).from(:N).to(:E)
+        end
+
+        specify '360 degrees' do
+          expect do
+            4.times do
+              rover.spin_left
+            end
+          end.not_to change(rover, :compass_point)
+        end
       end
 
-      it 'not change the y coordinate' do
-        expect{rover.go_west!}.not_to change(rover, :y)
+      describe 'spin right' do
+        specify '90 degrees' do
+          expect{rover.spin_right}.to change(rover, :compass_point).from(:N).to(:E)
+        end
+
+        specify '180 degrees' do
+          expect do
+            2.times do
+              rover.spin_right
+            end
+          end.to change(rover, :compass_point).from(:N).to(:S)
+        end
+
+        specify '270 degrees' do
+          expect do
+            3.times do
+              rover.spin_right
+            end
+          end.to change(rover, :compass_point).from(:N).to(:W)
+        end
+
+        specify '360 degrees' do
+          expect do
+            4.times do
+              rover.spin_left
+            end
+          end.not_to change(rover, :compass_point)
+        end
       end
     end
-
-    describe 'north' do
-      it 'not change x coordinate' do
-        expect{rover.go_north!}.not_to change(rover, :x)
-      end
-
-      it 'increments the y coordinate' do
-        expect{rover.go_north!}.to change(rover, :y).by(1)
-      end
-    end
-
-    describe 'south' do
-      it 'not change x coordinate' do
-        expect{rover.go_north!}.not_to change(rover, :x)
-      end
-
-      it 'decrements the y coordinate' do
-        expect{rover.go_south!}.to change(rover, :y).by(-1)
-      end
-    end
-
   end
 
 end
